9da925c1b45b3565dc111cc084e651d0f33ce933
Davi Baptista <daviarcosbaptista@gmail.com>
2024-08-20 13:51:08 -0300

    Feat: Menu responsivo

--- js/index.js
+++ js/index.js
@@ -1,8 +1,24 @@
 var hpBar = document.getElementById("hp-bar");
 var hpText = document.querySelector(".lifecount");
+var menuResponsive = document.querySelector('.menu-responsive');
+var closeMenu = document.querySelector('.close-menu');
+var nav = document.querySelector('nav');
+var backgroundBlur = document.querySelector('.background-blur');
 const portrait = document.querySelector('.portrait');
 
 portrait.addEventListener('click', (e) => {
     hpBar.style.width = "130px";
     hpText.innerHTML = "100";
 });
+
+menuResponsive.addEventListener('click', (e) => {
+    nav.style.left = "0px";
+    backgroundBlur.style.opacity = ".8";
+    backgroundBlur.style.pointerEvents = "auto";
+});
+
+closeMenu.addEventListener('click', (e) => {
+    nav.style.left = "-250px";
+    backgroundBlur.style.opacity = "0";
+    backgroundBlur.style.pointerEvents = "none";
+})
