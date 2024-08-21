3c9cd08f76708ab19a834555356f93a8f5893fc6
Davi Baptista <daviarcosbaptista@gmail.com>
2024-08-21 14:49:27 -0300

    Feat: minigame secreto

--- js/index.js
+++ js/index.js
@@ -1,15 +1,37 @@
-var hpBar = document.getElementById("hp-bar");
-var hpText = document.querySelector(".lifecount");
 var menuResponsive = document.querySelector('.menu-responsive');
 var closeMenu = document.querySelector('.close-menu');
 var nav = document.querySelector('nav');
 var backgroundBlur = document.querySelector('.background-blur');
 const portrait = document.querySelector('.portrait');
+const gameSecret = document.querySelector('.game-secret');
+var currentValue = 100;
+
+/* loadingbar.js feito por js */
+
+var bar1 = new ldBar("#hpBar");
+/* ldBar stored in the element */
+var bar2 = document.getElementById('hpBar').ldBar;
+bar1.set(currentValue);
+
+/* Interação com HP */
 
 portrait.addEventListener('click', (e) => {
-    hpBar.style.width = "130px";
-    hpText.innerHTML = "100";
+    bar1.set(currentValue-= 25);
+
+    if (currentValue == 0) {
+        console.log('oi')
+        portrait.classList.add('blurred');
+        gameSecret.classList.add('game-unlock')
+    }
 });
+
+/* abrir página secreta */
+
+gameSecret.addEventListener('click', (e) => {
+    window.location = "../pages/easter-egg.html"
+});
+
+/* Menu Reponsivo */
 
 menuResponsive.addEventListener('click', (e) => {
     nav.style.left = "0px";
