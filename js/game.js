038eefc314a7e4ef6e4c38601f1cbc02d4740312
Davi Baptista <daviarcosbaptista@gmail.com>
2024-08-23 16:19:52 -0300

    Feat: Easter egg continuando

--- js/game.js
+++ js/game.js
@@ -1,4 +1,18 @@
-var bar1 = new ldBar("#hpBar");
-/* ldBar stored in the element */
-var bar2 = document.getElementById('hpBar').ldBar;
-bar1.set(currentValue);
+const gd = document.querySelector('.gd');
+const gdPointsText = document.querySelector('.points-all')
+let gdPoints = 0;
+let gdPoints = 0;
+
+const upgradeEscrita = document.querySelector('#escrita');
+const upgradeDocumentacao = document.querySelector('#documentacao');
+const upgradePlanilhas = document.querySelector('#planilhas');
+const upgradeBalanceamento = document.querySelector('#balanceamento');
+
+/* Ganhar GD Points */
+gd.addEventListener('click', () => {
+    gdPoints += 1;
+    gdPointsText.innerHTML = gdPoints + " GD Points"
+});
+
+
+/* upgrades */
